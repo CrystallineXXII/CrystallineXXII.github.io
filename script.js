fe39e9523d75ca49524228f8bf885d96b191af02
joppukutten@gmail.com <joppukutten@gmail.com>
2023-10-05 00:15:15 +0530

    4Oct-Update-2

---
+++ script.js
@@ -0,0 +1,19 @@
+let mainHeader = document.getElementsByClassName ("main-header")[0];
+let changed = true;
+
+window.addEventListener("scroll", function(event){
+  if (this.scrollY > 20 && changed == false){
+    mainHeader.classList.remove("main-header")
+    mainHeader.classList.add("post-scroll-main-header");
+    
+    changed = true
+  }
+  if(this.scrollY < 20 && changed == true){
+  mainHeader.classList.remove("post-scroll-main-header")
+  mainHeader.classList.add("main-header");
+  
+  changed = false
+  }
+  
+  console.log(this.scrollY,changed,mainHeader.classList);
+});
