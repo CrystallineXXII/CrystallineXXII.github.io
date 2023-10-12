0724b6e52f8abc5307b9ae0c05a184d051eaaef9
Joseph's Mac <joppukutten@gmail.com>
2023-10-13 00:50:27 +0530

    Added Records

--- jqry.js
+++ jqry.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-  $('#crystalline_curated').click(function(){
-    $('#spotify').slideToggle()
+$(document).ready(function () {
+
+  $("#spotify").slideUp();
+  $("#record").slideUp();
+
+  $("#crystalline_curated").click(function () {
+    $("#spotify").slideToggle();
+  });
+  $("#record_heading").click(function () {
+    $("#record").slideToggle(2000);
   });
 });
